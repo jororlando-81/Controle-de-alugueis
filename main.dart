8c7eb36a56d0851d94057a9884ce2bc24f75aad1
jororlando-81 <80168257+jororlando-81@users.noreply.github.com>
2022-05-23 19:57:38 -0300

    Adição de paginas

---
+++ main.dart
@@ -0,0 +1,150 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Controle de Alugueis', //título da aplicação
+      debugShowCheckedModeBanner: false, //Tira a Barrinha de debug da tela
+      theme: ThemeData(primarySwatch: Colors.green), //Altera a cor principal
+      home:
+          const MyStatefulWidget(title: 'Escolha uma função'), //título da tela
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  @override
+  Widget build(BuildContext context) {
+    final ButtonStyle style =
+        ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20));
+
+    return Scaffold(
+        appBar: AppBar(
+          toolbarHeight: 180.0,
+          title: Text(widget.title),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              SizedBox(
+                width: 963.0,
+                height: 150.0,
+                child: ElevatedButton.icon(
+                  label: const Text('    ADICIONAR ALUGUEL'),
+                  icon: Icon(
+                    Icons.house,
+                    size: 92.0,
+                    color: Colors.greenAccent.shade400,
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    onPrimary: Colors.black, // muda a cor das letras do botao
+                    primary: Colors.white,
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(100.0)),
+                      side: BorderSide(color: Colors.green, width: 9),
+                    ),
+                    textStyle: TextStyle(fontSize: 60),
+                  ),
+                  onPressed: () {
+
+                       Navigator.push(
+                       context,
+                       MaterialPageRoute(builder: (context) => const Pag2()),
+            );
+
+
+                  },
+                ),
+              ),
+              SizedBox(height: 50),
+              SizedBox(
+                width: 963.0,
+                height: 150.0,
+                child: ElevatedButton.icon(
+                  label: const Text('    MEUS ALUGUEIS'),
+                  icon: Icon(
+                    Icons.list,
+                    size: 114.0,
+                    color: Colors.greenAccent.shade400,
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    onPrimary: Colors.black, // muda a cor das letras do botao
+                    primary: Colors.white,
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(100.0)),
+                      side: BorderSide(color: Colors.green, width: 9),
+                    ),
+                    textStyle: TextStyle(fontSize: 60),
+                  ),
+                  onPressed: () {
+                  
+                      Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const Pag3()),
+              ); 
+                  }
+                ),
+              ),
+            ],
+          ),
+        ));
+  }
+}
+
+class Pag2 extends StatelessWidget {
+  const Pag2({Key? key}) : super(key: key); 
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Criar aluguel'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('Voltar!'),
+        ),
+      ),
+    );
+  }
+}
+
+class Pag3 extends StatelessWidget {
+  const Pag3({Key? key}) : super(key: key); 
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Alugueis cadastrados'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('Voltar!'),
+        ),
+      ),
+    );
+  }
+}
